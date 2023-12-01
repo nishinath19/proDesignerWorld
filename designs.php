36bb9108007862dc772599d2071245846aaae2d4
atiq1997 <rahmanatiq350@gmail.com>
2023-12-01 21:32:57 +0600

    ne

---
+++ designs.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<section class="my-5 ">
+        <div class='py-5'>
+            <h2 class="text-center">Our Servicess</h2>
+        </div>
+        <div class='container-fluid'>
+
+
+
+            <div class='row'>
+                <div class='col-lg-4 col-md-4 col-12'>
+                  <div class="card" > 
+                    <img src="images/BC 005.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    </div>
+                </div>
+                <div class='col-lg-4 col-md-4 col-12'>
+                  <div class="card " >
+                    <img src="images/flayer.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    </div>
+                </div>
+                <div class='col-lg-4 col-md-4 col-12'>
+                  <div class="card" >
+                    <img src="images/cci.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+</br>
+
+
+
+            <div class='row'>
+                <div class='col-lg-4 col-md-4 col-12'>
+                  <div class="card" >
+                    <img src="images/Ei.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    </div>
+                </div>
+                
+                <div class='col-lg-4 col-md-4 col-12'>
+                  <div class="card " >
+                    <img src="images/Ci.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    </div>
+                </div>
+
+                <div class='col-lg-4 col-md-4 col-12'>
+                  <div class="card" >
+                    <img src="images/logo 2.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    </div>
+                </div>
+              </div>
+
+
+
+        </div>
+
+    </section>
+</body>
+</html>
--- index.php
+++ index.php
@@ -123,7 +123,10 @@
             <div class='row'>
                 <div class='col-lg-4 col-md-4 col-12'>
                   <div class="card" > 
-                    <img src="images/BC 005.png" class="card-img-top" alt="...">
+                    <a href="designs.php">
+                    <img src="images/BC 005.png" class="card-img-top" alt="..." >
+                    </a>
+                    
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
