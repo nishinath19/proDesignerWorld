6799537fbc0c9490a4d9b4be12e5227867f33fa2
No-one-GOT <mobarak.hossain1936@gmail.com>
2023-12-01 23:17:15 +0600

    add commit

---
+++ about.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pro Designer World</title>
+    
+    <link rel="Icon" type="Images/x-icon" href="images/logo.png">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/about.css">
+
+</head>
+
+<body class="m-3 mt-4">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark-subtle ">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Pro Designer World</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 m-md-auto ">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Designs</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">contacts</a>
+            </li>
+            
+        </ul>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        </div>
+    </div>
+</nav>
+<div>
+    <h2>Our jouny</h2>
+    <div class="board ">
+        <h2>board Of Directors</h2>
+    </div>
+    <div class="row col-12">
+        <div class="col-4 col-xs-12 col-sm-4 h-300">
+                        <div class="card">
+                            <div class="card-header">
+                                <img src="images/pic/Nishinath 5   5(1).jpg" alt="" class="image_col">
+                            </div>
+                            <div class="card-body">
+                                <h1>Nishhinath Chondro Gain</h1>
+                                <a href="">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+        </div>
+        <div class="col-4 col-xs-12 col-sm-4 h-300">
+                        <div class="card">
+                            <div class="card-header">
+                                <img src="images/pic/Nishinath 5   5(1).jpg" alt="" class="image_col">
+                            </div>
+                            <div class="card-body">
+                                <h1>Nishhinath Chondro Gain</h1>
+                                <a href="">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+        </div>
+        <div class="col-4 col-xs-12 col-sm-4 h-300">
+                        <div class="card">
+                            <div class="card-header">
+                                <img src="images/pic/Nishinath 5   5(1).jpg" alt="" class="image_col">
+                            </div>
+                            <div class="card-body">
+                                <h1>Nishhinath Chondro Gain</h1>
+                                <a href="">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+        </div>
+        
+    </div>
+
+</div>
+
+
+
+
+
+
+
+</body>
