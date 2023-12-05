8dd7833fe78571352165273bb8b3bce5ba256e6b
Nishinath Chondro Gain <nishinath19april@gmail.com>
2023-12-05 10:33:31 +0600

    dd

--- userinfo.php
+++ userinfo.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
 $con = mysqli_connect("localhost","root","Dayal019")
 
@@ -30,4 +30,29 @@ mysqli_query($con, $query);
 header("loacation:index.php");
 
 
+?> -->
+
+<?php
+	$user = $_POST['user'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $comments = $_POST['comments'];
+
+
+	// Database connection
+	$conn = new mysqli('localhost','root','Dayal019','youtubeuserdata');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(user, email, mobile, comments) values(?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $user, $email, $mobile, $comments);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+
+    echo "$query";
 ?>
